234936fc88b1ec50946403bce4ced76b7d1e8ca2
Sekers <46898253+Sekers@users.noreply.github.com>
2023-01-19 15:38:37 -0600

    CHECK FOR EMPTY/NULL BEFORE REPAIRING API DATE

--- SKYAPI/Functions/Get-SchoolUserExtended.ps1
+++ SKYAPI/Functions/Get-SchoolUserExtended.ps1
@@ -63,11 +63,12 @@ function Get-SchoolUserExtended
         # Fix date-only fields since the API returns dates with improper time values.
         # NOTE: DO NOT FIX THE FOLLOWING AS THEY HAVE PROPER TIMES:
         #  - audit_date
-        $response.deceased_date = Repair-SkyApiDate -Date $response.deceased_date
-        $response.birth_date = Repair-SkyApiDate -Date $response.birth_date
-        $response.depart_date = Repair-SkyApiDate -Date $response.depart_date
-        $response.enroll_date = Repair-SkyApiDate -Date $response.enroll_date
-        $response.last_sync_date = Repair-SkyApiDate -Date $response.last_sync_date # Not 100% sure if I need to fix this one since it might actually be storing time information (I don't have a record with this set properly to test). However, I don't think this stores time so we are probably good.
+        if (-not [string]::IsNullOrEmpty($response.deceased_date)){$response.deceased_date = Repair-SkyApiDate -Date $response.deceased_date}
+        if (-not [string]::IsNullOrEmpty($response.birth_date)){$response.birth_date = Repair-SkyApiDate -Date $response.birth_date}
+        if (-not [string]::IsNullOrEmpty($response.depart_date)){$response.depart_date = Repair-SkyApiDate -Date $response.depart_date}
+        if (-not [string]::IsNullOrEmpty($response.enroll_date)){$response.enroll_date = Repair-SkyApiDate -Date $response.enroll_date}
+        # Not 100% sure if I need to fix last_sync_date since it might actually be storing time information (I don't have a record with this set properly to test). However, I don't think this stores time so we are probably good.
+        if (-not [string]::IsNullOrEmpty($response.last_sync_date)){$response.last_sync_date = Repair-SkyApiDate -Date $response.last_sync_date}
 
         # TODO - There is a bug with the dates not appearing correctly on the website when submitting occupations on this endpoint. I have a support request open with Blackbaud
         #        They were able to replicate the issue and have passed it on to the product development team for further review.
@@ -75,11 +76,11 @@ function Get-SchoolUserExtended
         {
             $index = $response.occupations.IndexOf($occupation)
             
-            $response.occupations[$index].begin_date = Repair-SkyApiDate -Date $occupation.begin_date
-            $response.occupations[$index].end_date = Repair-SkyApiDate -Date $occupation.end_date
+            if (-not [string]::IsNullOrEmpty($occupation.begin_date)){$response.occupations[$index].begin_date = Repair-SkyApiDate -Date $occupation.begin_date}
+                if (-not [string]::IsNullOrEmpty($occupation.end_date)){$response.occupations[$index].end_date = Repair-SkyApiDate -Date $occupation.end_date}
         }
 
-        $response.retire_date = Repair-SkyApiDate -Date $response.retire_date
+        if (-not [string]::IsNullOrEmpty($response.retire_date)){$response.retire_date = Repair-SkyApiDate -Date $response.retire_date}
 
         $response 
     }
